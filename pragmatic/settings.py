259ca0ac38e944c4b08490debb23b80016bc257d
박관우 <dirn0568@naver.com>
2021-06-03 14:15:26 +0900

    django course 29 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -147,4 +147,9 @@ LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
